cb1ad83c5f7a42a2a4cee1e27d59080db9ab95c1
Sangram <panda.sangram20@gmail.com>
2023-02-11 20:32:31 +0530

    config the size of window

--- main.py
+++ main.py
@@ -2,26 +2,21 @@ import requests
 from tkinter import *
 
 
-
-window=Tk()
+window = Tk()
 window.title("Domain checker")
-window.config(padx=20,pady=20)
-canvas=Canvas(window,width=500,height=500)
+window.config(padx=20, pady=20)
+window.minsize(width=300, height=300)
+
 
 def Entry_domain():
-    domain = input(input_domain.get())
-    response = requests.get(url=f"https://api.whoisfreaks.com/v1.0/whois?apiKey=65a7b9e7b5204eff8ca880e216cdc2b4&whois=live&domainName={domain}")
+    domain = str(input_domain.get())
+    response = requests.get(
+        url=f"https://api.whoisfreaks.com/v1.0/whois?apiKey=65a7b9e7b5204eff8ca880e216cdc2b4&whois=live&domainName={domain}")
     res = response.json()
     status = res["domain_registered"]
 
 
-    
+input_domain = Entry(width=10)
+input_domain.grid(column=1, row=2)
 
-if status == "yes":
-    msg = "Sorry Domain can already tacken"
-else:
-    msg = "Hurry it's avlevel !"
-
-print(msg)
-
-
+window.mainloop()
