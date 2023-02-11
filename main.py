b9484d31e38883420c8daf4da7bdb419602316f0
Sangram <panda.sangram20@gmail.com>
2023-02-11 21:23:08 +0530

    add its approch to retuen result

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def Entry_domain():
         url=f"https://api.whoisfreaks.com/v1.0/whois?apiKey=65a7b9e7b5204eff8ca880e216cdc2b4&whois=live&domainName={domain}")
     res = response.json()
     status = res["domain_registered"]
+    if status =="yes":
+        result="!ohoo it will be already taken"
+    else:
+        result="Huryy its available"
+    label3.config(text=result)
 
 
 # lable for text show 
@@ -28,10 +33,16 @@ input_domain = Entry(width=50)
 input_domain.grid(column=2, row=5)
 
 #  add a submit Button
-check=Button(text="Check",background="lightgreen",width=20)
+check=Button(text="Check",background="lightgreen",width=20,borderwidth=3,font=("Sans Serif",10,"bold"),command=Entry_domain)
 check.grid(column=2,row=7)
+# lable for  show  result
+label3 = Label(text="", font=("Sans Serif",22,"bold"))
+label3.grid(column=2, row=9)
+
+
 # for placeholder the text
 input_domain.insert(0, "eg. web-angle.com")
+
 
 
 window.mainloop()
