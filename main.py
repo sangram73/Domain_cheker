e895254568413adacbaafbb0b838b8fd2d818a3a
Sangram <panda.sangram20@gmail.com>
2023-02-11 19:23:22 +0530

    intilization of  function that can take input gives output as a json

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ window=Tk()
 window.title("Domain checker")
 window.config(padx=20,pady=20)
 canvas=Canvas(window,width=500,height=500)
-domain = input("check domain name: ")
-response = requests.get(
-    url=f"https://api.whoisfreaks.com/v1.0/whois?apiKey=65a7b9e7b5204eff8ca880e216cdc2b4&whois=live&domainName={domain}")
-res = response.json()
-status = res["domain_registered"]
+
+def Entry_domain():
+    domain = input(input_domain.get())
+    response = requests.get(url=f"https://api.whoisfreaks.com/v1.0/whois?apiKey=65a7b9e7b5204eff8ca880e216cdc2b4&whois=live&domainName={domain}")
+    res = response.json()
+    status = res["domain_registered"]
+
+
+    
 
 if status == "yes":
     msg = "Sorry Domain can already tacken"
