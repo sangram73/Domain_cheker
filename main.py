c9f6ba9f926ba233b5536c18ed9bb830cef1b1df
Sangram <panda.sangram20@gmail.com>
2023-02-11 20:44:19 +0530

    adding some placeholder text

--- main.py
+++ main.py
@@ -16,7 +16,15 @@ def Entry_domain():
     status = res["domain_registered"]
 
 
-input_domain = Entry(width=10)
-input_domain.grid(column=1, row=2)
+# lable for text show 
+label1 = Label(text="Useing Tkinter", font=("Arial",30,"bold"))
+label1.grid(column=2, row=0)
+
+# entry for get input for user
+input_domain = Entry(width=40,highlightcolor="#3b3a30")
+input_domain.grid(column=2, row=1)
+# for placeholder the text
+input_domain.insert(0, "eg. web-angle.com")
+
 
 window.mainloop()
